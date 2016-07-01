070e868ea7e812e6ae8adf252c98f7ba74da8687
Андрей Овчаренко <ovcharenkoav@meta.ua>
2016-07-01 16:02:07 +0300

    static

--- Readme.md
+++ Readme.md
@@ -1,2 +1,3 @@
 mvn clean compile
 mvn clean test
+http://vertx.io/blog/some-rest-with-vert-x/
--- src/main/java/io/vertx/blog/first/MyFirstVerticle.java
+++ src/main/java/io/vertx/blog/first/MyFirstVerticle.java
@@ -1,24 +1,57 @@
 package io.vertx.blog.first;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+//import io.vertx.ext.mongo.MongoClient;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.StaticHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 public class MyFirstVerticle extends AbstractVerticle {
 
 @Override
 public void start(Future<Void> fut) {
+        // Create a router object.
+        Router router = Router.router(vertx);
+
+        // Bind "/" to our hello message - so we are still compatible.
+        router.route("/").handler(routingContext -> {
+                                          HttpServerResponse response = routingContext.response();
+                                          response
+                                          .putHeader("content-type", "text/html")
+                                          .end("<h1>Hello from my first Vert.x 3 application</h1>");
+                                  });
+
+
+        // Serve static resources from the /assets directory
+        router.route("/assets/*").handler(StaticHandler.create("assets"));
+        // Create the HTTP server and pass the "accept" method to the request handler.
         vertx
         .createHttpServer()
-        .requestHandler(r -> {
-                                r.response().end("<h1>Hello from my first " +
-                                                 "Vert.x 3 application</h1>");
-                        })
-        .listen(config().getInteger("http.port", 8080), result -> {
+        .requestHandler(router::accept)
+        .listen(
+                // Retrieve the port from the configuration,
+                // default to 8080.
+                config().getInteger("http.port", 8080),
+                result -> {
                         if (result.succeeded()) {
                                 fut.complete();
                         } else {
                                 fut.fail(result.cause());
                         }
-                });
+                }
+                );
 }
+
 }
